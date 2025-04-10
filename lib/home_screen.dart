5ce3c62e05e4637fb4fabd3c6b58c1ba3cafcfbd
Avinash Magar <avinashmagar15@gmail.com>
2025-04-10 23:09:52 +0530

    new code addde

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +9,45 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Api Integration')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final json = {
+            "userId": 1,
+            "id": 1,
+            "title":
+                "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            "body":
+                "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+          };
+          PostModel postModel = PostModel.fromJson(json);
+          print(postModel.title);
+        },
+      ),
+    );
+  }
+}
+
+class PostModel {
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+
+  PostModel({
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
+
+  factory PostModel.fromJson(Map<String, dynamic> json) {
+    return PostModel(
+      id: json['id'],
+      userId: json['userId'],
+      title: json['title'],
+      body: json['body'],
+    );
   }
 }
